9a1b3afd2c920fc5c697afb876bcd6f3a2b0bc3d
caiyuhong <cai1083088795@gmail.com>
2021-06-18 09:12:58 +0800

    升级依赖库

--- buildPlugin/src/main/kotlin/com/clistery/gradle/AppVersion.kt
+++ buildPlugin/src/main/kotlin/com/clistery/gradle/AppVersion.kt
@@ -5,7 +5,7 @@ package com.clistery.gradle
 object AppVersion {
 
     object clistery{
-        const val appinject = "1.3.6.1"
+        const val appinject = "1.3.6.2"
         const val kotlin_realm_ext = "3.0.3"
     }
 
