b9359e2ba2f9c67c3f8db4ff6622de1fb75955b3
caiyuhong <cai1083088795@gmail.com>
2021-06-22 10:53:39 +0800

    降低sdk版本

--- buildPlugin/src/main/kotlin/com/clistery/gradle/AppConfig.kt
+++ buildPlugin/src/main/kotlin/com/clistery/gradle/AppConfig.kt
@@ -1,10 +1,10 @@
 package com.clistery.gradle
 
 object AppConfig {
-    const val compileSdk = 30
-    const val buildToolsVersion = "30.0.3"
+    const val compileSdk = 28
+    const val buildToolsVersion = "28.0.3"
     const val minSdk = 19
-    const val targetSdk = 30
+    const val targetSdk = 28
     
     const val GROUP_ID = "com.clistery.app"
     const val ARTIFACT_ID = "callrecord"
