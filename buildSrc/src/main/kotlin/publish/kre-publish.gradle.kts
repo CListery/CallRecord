6c1e3668f59b9d4114c44ae4259e472c9d4cb53d
caiyuhong <cai1083088795@gmail.com>
2023-11-30 15:10:22 +0800

    移除dokka文档

--- buildSrc/src/main/kotlin/publish/kre-publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish/kre-publish.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("lib") apply true
-    id("org.jetbrains.dokka")
+    // id("org.jetbrains.dokka")
     `maven-publish`
     signing
 }
@@ -12,26 +12,26 @@ val androidSourcesJar by tasks.register<Jar>("androidSourcesJar") {
     archiveClassifier.set("sources")
 }
 
-tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
-    val artifactId: String? by project
-    moduleName.set(artifactId)
-    
-    dokkaSourceSets {
-        configureEach {
-            skipDeprecated.set(true)
-            skipEmptyPackages.set(true)
-            //            noStdlibLink.set(true)
-            //            noAndroidSdkLink.set(true)
-            //            noJdkLink.set(true)
-            //            includeNonPublic.set(true)
-        }
-    }
-    offlineMode.set(true)
-}
+// tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
+//     val artifactId: String? by project
+//     moduleName.set(artifactId)
+//
+//     dokkaSourceSets {
+//         configureEach {
+//             skipDeprecated.set(true)
+//             skipEmptyPackages.set(true)
+//             //            noStdlibLink.set(true)
+//             //            noAndroidSdkLink.set(true)
+//             //            noJdkLink.set(true)
+//             //            includeNonPublic.set(true)
+//         }
+//     }
+//     offlineMode.set(true)
+// }
 
 val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
-    dependsOn(tasks.dokkaHtml)
-    from(tasks.dokkaHtml.get().outputDirectory)
+    // dependsOn(tasks.dokkaHtml)
+    // from(tasks.dokkaHtml.get().outputDirectory)
     archiveClassifier.set("javadoc")
 }
 
@@ -148,7 +148,7 @@ tasks.withType<Sign>().configureEach {
     onlyIf { isReleaseVersion }
 }
 
-tasks.register<org.jetbrains.dokka.gradle.DokkaTask>("dokkaDocs") {
-    dependsOn(tasks.dokkaJekyll)
-    outputDirectory.set(file("$rootDir/docs"))
-}
+// tasks.register<org.jetbrains.dokka.gradle.DokkaTask>("dokkaDocs") {
+//     dependsOn(tasks.dokkaJekyll)
+//     outputDirectory.set(file("$rootDir/docs"))
+// }
